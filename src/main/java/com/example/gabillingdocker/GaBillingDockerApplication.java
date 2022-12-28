b46b05f2d1b841b3732d988d62fc0080ce6e79d9
David Pc Razer <info@david-dupuis.be>
2022-12-28 20:51:29 +0100

    First Commit

---
+++ src/main/java/com/example/gabillingdocker/GaBillingDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gabillingdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaBillingDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaBillingDockerApplication.class, args);
+    }
+
+}
